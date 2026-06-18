4cc1560b982592ecb72bed139ca11ae9139c86b5
DiamondPCalh <envileeaistudios@outlook.com>
2026-06-18 03:26:17 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,20 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export function middleware(request: NextRequest) {
-  return NextResponse.next()
-}
+const isProtectedRoute = createRouteMatcher([
+  '/pod(.*)',
+  '/ai-studios(.*)',
+  '/music(.*)',
+  '/academy(.*)',
+  '/suite(.*)',
+  '/legal(.*)',
+  '/prompts(.*)',
+  '/vault(.*)',
+])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+})
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-  ],
+  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)', '/(api|trpc)(.*)'],
 }
